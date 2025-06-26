2df52427acb446e515bba8df56388e7eefc8c520
Ryoheus <Ryoheus@users.noreply.github.com>
2025-06-26 11:53:52 +0900

    UIの追加

--- frmichiran_gyousha.vb
+++ frmichiran_gyousha.vb
@@ -1,5 +1,25 @@
 ﻿Public Class frmichiran_gyousha
     Private Sub frmichiran_gyousha_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        set_gyousha_ichiran()
+    End Sub
+
+    Private Sub btn_modoru_Click(sender As Object, e As EventArgs) Handles btn_modoru.Click
+        Me.Close() : Me.Dispose()
+    End Sub
+
+    Private Sub btn_touroku_Click(sender As Object, e As EventArgs) Handles btn_touroku.Click
+
+    End Sub
+
+    Private Sub btn_henkou_Click(sender As Object, e As EventArgs) Handles btn_henkou.Click
+
+    End Sub
+
+    Private Sub btn_sakujo_Click(sender As Object, e As EventArgs) Handles btn_sakujo.Click
+
+    End Sub
+
+    Private Sub set_gyousha_ichiran()
 
     End Sub
 End Class
