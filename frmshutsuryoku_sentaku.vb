d62f86db772d733810efd3e4c821b698d1cd20ee
Ryoheus <Ryoheus@users.noreply.github.com>
2025-07-01 09:27:16 +0900

    整理

--- frmshutsuryoku_sentaku.vb
+++ frmshutsuryoku_sentaku.vb
@@ -1,8 +1,26 @@
 ﻿Public Class frmshutsuryoku_sentaku
     Private Sub btn_modoru_Click(sender As Object, e As EventArgs) Handles btn_modoru.Click
-
         Me.Close()
         Me.Dispose()
+    End Sub
+
+    Private Sub btn_shouhin_Click(sender As Object, e As EventArgs) Handles btn_shouhin.Click
+
+    End Sub
+
+    Private Sub btn_tenpo_Click(sender As Object, e As EventArgs) Handles btn_tenpo.Click
+
+    End Sub
+
+    Private Sub btn_kurikoshizan_Click(sender As Object, e As EventArgs) Handles btn_kurikoshizan.Click
+
+    End Sub
+
+    Private Sub btn_uera_hanbai_jisseki_Click(sender As Object, e As EventArgs) Handles btn_uera_hanbai_jisseki.Click
+
+    End Sub
+
+    Private Sub btn_uera_shouhin_zaiko_Click(sender As Object, e As EventArgs) Handles btn_uera_shouhin_zaiko.Click
 
     End Sub
 End Class
