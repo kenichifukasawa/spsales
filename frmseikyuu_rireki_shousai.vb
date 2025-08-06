b3426501157faae5635cbab894fe37748e1c47ed
Ryoheus <Ryoheus@users.noreply.github.com>
2025-08-06 23:13:02 +0900

    請求書履歴の詳細ボタンの追加（途中）

---
+++ frmseikyuu_rireki_shousai.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmseikyuu_rireki_shousai
+    Private Sub btn_modoru_Click(sender As Object, e As EventArgs) Handles btn_modoru.Click
+        Me.Close() : Me.Dispose()
+    End Sub
+
+    Private Sub btn_koushin_Click(sender As Object, e As EventArgs) Handles btn_koushin.Click
+
+    End Sub
+End Class
