775fe0faba8dc2f3095eefa5690b6208988f6387
Ryoheus <Ryoheus@users.noreply.github.com>
2025-08-14 22:32:59 +0900

    支払伝票の登録コードの追加

--- frmshiharai_shori.vb
+++ frmshiharai_shori.vb
@@ -18,6 +18,171 @@ Public Class frmshiharai_shori
 
     Private Sub btn_touroku_Click(sender As Object, e As EventArgs) Handles btn_touroku.Click
 
+        Dim dgv = dgv_kensakukekka_shiire
+        Dim can = False
+        For i = 0 To dgv.Rows.Count - 1
+            If dgv.Rows(i).Cells(0).Value = True Then
+                can = True
+            End If
+        Next
+        If Not can Then
+            msg_go("関連にしたい項目を選択してから実行してください。")
+            Exit Sub
+        End If
+
+        Dim gyousha_id = Mid(Trim(cbx_gyousha.Text), 1, 6)
+        If gyousha_id = "" Then
+            msg_go("業者を選択してください。")
+            Exit Sub
+        End If
+
+        Dim houhou = Trim(cbx_shiharai_houhou.Text)
+        If houhou = "" Then
+            msg_go("方法を選択してください。")
+            Exit Sub
+        End If
+        Dim houhou_id = PaymentMethodsInvoice.GetIdByName(houhou)
+
+        Dim shiharai_kingaku = Trim(txt_kingaku.Text)
+        If shiharai_kingaku = "" Then
+            msg_go("金額を入力してください。")
+            Exit Sub
+        End If
+        Dim int_shiharai_kingaku = CInt(shiharai_kingaku)
+
+        Dim id_busy = 2
+        Dim s_no_busy = 4
+        Dim get_response_busy = get_settings(id:=id_busy, s_no:=s_no_busy)
+        If get_response_busy = "" Then
+            msg_go("ビジーの取得に失敗しました。")
+            Exit Sub
+        Else
+
+            If get_response_busy <> "0" Then
+                msg_go("サーバがビジーです。少ししてから再度実行してください。")
+                Exit Sub
+            End If
+
+            Dim update_response = update_settings(id:=id_busy, s_no:=s_no_busy, new_value:="1")
+            If Not update_response Then
+                msg_go("ビジーの更新に失敗しました。")
+                Exit Sub
+            End If
+
+        End If
+
+        Dim shiharaibi = dtp_shiharai_bi.Value.ToString("yyyyMMdd")
+            Dim kijitsu = dtp_shiharai_bi.Value.ToString("yyyyMMdd")
+        Dim bikou = Trim(txt_bikou.Text)
+
+        Dim id = 1
+        Dim s_no = 14
+        Dim ketasuu = 8
+        Dim new_id = get_settings(id:=id, s_no:=s_no)
+        Dim next_id As String
+        If new_id = "" Then
+            msg_go("IDの取得に失敗しました。")
+            Exit Sub
+        ElseIf new_id = "0" Then
+            next_id = "2"
+            new_id = 1.ToString("D" + ketasuu.ToString)
+        Else
+            next_id = (CLng(new_id) + 1).ToString
+            new_id = new_id.ToString.PadLeft(ketasuu, "0"c)
+        End If
+
+        Dim response = update_settings(id:=id, s_no:=s_no, new_value:=next_id)
+        If Not response Then
+            msg_go("IDの更新に失敗しました。")
+            Exit Sub
+        End If
+
+        Try
+
+            Dim cn_server As New SqlConnection
+            cn_server.ConnectionString = connectionstring_sqlserver
+
+            Dim query = "SELECT * FROM shukkin"
+
+            Dim da As SqlDataAdapter = New SqlDataAdapter(query, cn_server)
+            Dim ds As New DataSet
+            Dim temp_table_name = "t_shukkin"
+            da.Fill(ds, temp_table_name)
+            Dim cb As SqlClient.SqlCommandBuilder = New SqlClient.SqlCommandBuilder(da)
+            Dim data_row As DataRow = ds.Tables(temp_table_name).NewRow()
+
+            data_row("shukkinid") = new_id
+            data_row("gyoushaid") = gyousha_id
+            data_row("shukkinbi") = shiharaibi
+            data_row("shukkingaku") = int_shiharai_kingaku
+            data_row("shukkinhouhou") = houhou_id
+
+            If bikou <> "" Then
+                data_row("bikou") = bikou
+            End If
+
+            If kijitsu <> "" Then
+                data_row("kijitsu") = kijitsu
+            End If
+
+            ds.Tables(temp_table_name).Rows.Add(data_row)
+            da.Update(ds, temp_table_name)
+            ds.Clear()
+
+        Catch ex As Exception
+            msg_go(ex.Message)
+            Exit Sub
+        End Try
+
+        For i = 0 To dgv.Rows.Count - 1
+
+            If dgv.Rows(i).Cells(0).Value = False Then
+                Continue For
+            End If
+
+            Dim shiireid = dgv.Rows(i).Cells(2).Value
+
+            Try
+
+                Dim conn As New SqlConnection
+                conn.ConnectionString = connectionstring_sqlserver
+
+                Dim query = "SELECT * FROM shiire WHERE shiireid = '" + shiireid + "'"
+
+                Dim da As New SqlDataAdapter
+                da = New SqlDataAdapter(query, conn)
+                Dim ds As New DataSet
+                Dim temp_table_name = "t_shiire"
+                da.Fill(ds, temp_table_name)
+
+                Dim table = ds.Tables(temp_table_name)
+
+                table.Rows(0)("shukkinid") = new_id
+                table.Rows(0)("shiharaibi") = shiharaibi
+                table.Rows(0)("joukyou") = "1"
+
+                Dim cb As New SqlCommandBuilder
+                cb.DataAdapter = da
+                da.Update(ds, temp_table_name)
+                ds.Clear()
+
+            Catch ex As Exception
+                msg_go(ex.Message)
+                Exit Sub
+            End Try
+
+        Next
+
+        Dim update_response_2 = update_settings(id:=id_busy, s_no:=s_no_busy, new_value:="0")
+        If Not update_response_2 Then
+            msg_go("ビジーの更新に失敗しました。")
+            Exit Sub
+        End If
+
+        msg_go("登録しました。", 64)
+
+        set_shuukei()
+
     End Sub
 
     Private Sub btn_shiire_shousai_Click(sender As Object, e As EventArgs) Handles btn_shiire_shousai.Click
